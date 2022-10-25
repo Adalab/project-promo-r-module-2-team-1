da7d68267675d384961e26f5aa431ac44d2fd7cb
marocena26 <mariaelena.arocena@gmail.com>
2022-10-25 14:10:55 +0200

    start js button reset

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// 1- Elementos de la página que voy a usar.
+
+const resetBtn = document.querySelector('.js-card__button');
+const profileName = document.querySelector ('.js-profile__name');
+const profileText = document.querySelector ('.js-profile__text');
+const linkMobile = document.querySelector ('.js-nav__li1');
+const linkEmail = document.querySelector ('.js-nav__li2');
+const linkLinkedin = document.querySelector ('.js-nav__li3');
+const linkGithub = document.querySelector ('.js-nav__li4');
+
+
+// 2- Variables globales: contiene los datos de la aplicación.
+
+const content = 
+
+// 3- Funciones.
+
+
+// 4- Eventos.
+
+resetBtn.addEventListener('click',(event) => {
+    event.preventDefault();
+    profileName.innerHTML = 
+  
+
+})
+
+//Botón de reset día 25/10
