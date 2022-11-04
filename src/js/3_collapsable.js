2429034f40fbebb41808093322de386aa06b47fd
Ana Lozano Moreno <analozanomor@gmail.com>
2022-11-04 13:23:57 +0100

    fix merge ticket/02

--- src/js/3_collapsable.js
+++ src/js/3_collapsable.js
@@ -2,95 +2,39 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 
-function showDesignClicker() {
-  sectionDesign.classList.remove('collapsed');
-}
-
-function hideDesignClicker() {
-  sectionDesign.classList.add('collapsed');
-}
-
-// ---------- FUNCION RELLENA -----------------
-function showFillClicker() {
-  sectionFill.classList.remove('collapsed');
-}
-
-function hideFillClicker() {
-  sectionFill.classList.add('collapsed');
-}
-
-// ---------- FUNCION COMPARTE -----------------
-
-function showShareClicker() {
-  sectionShare.classList.remove('collapsed');
-}
-
-function hideShareClicker() {
-  sectionShare.classList.add('collapsed');
-}
-
-// ---------- FUNCIONES MANEJADORAS -----------------
-//DISEÑA
-function handleClickDesing(event) {
+designClicker.addEventListener('click', (event) => {
   event.preventDefault();
 
   if (sectionDesign.classList.contains('collapsed')) {
-    showDesignClicker();
+    sectionDesign.classList.remove('collapsed');
     arrowDesign.classList.add('arrow-up');
     arrowDesign.classList.remove('arrow-down');
     closeFill();
     closeShare();
-    sectionFill.classList.add('collapsed');
-    sectionShare.classList.add('collapsed');
-  } else {
-    hideDesignClicker();
-    arrowDesign.classList.remove('arrow-up');
-    arrowDesign.classList.add('arrow-down');
-    sectionFill.classList.remove('collapsed');
-    sectionShare.classList.remove('collapsed');
   }
-}
-//RELLENA
-function handleClickFill(event) {
+});
+
+fillClicker.addEventListener('click', (event) => {
   event.preventDefault();
   if (sectionFill.classList.contains('collapsed')) {
-    showFillClicker();
+    sectionFill.classList.remove('collapsed');
     arrowFill.classList.add('arrow-up');
     arrowFill.classList.remove('arrow-down');
-
     closeDesign();
     closeShare();
-    sectionDesign.classList.add('collapsed');
-    sectionShare.classList.add('collapsed');
-  } else {
-    hideFillClicker();
-    arrowFill.classList.remove('arrow-up');
-    arrowFill.classList.add('arrow-down');
-    sectionDesign.classList.remove('collapsed');
-    sectionShare.classList.remove('collapsed');
   }
-}
-//COMPARTE
-function handleClickShare(event) {
+});
+
+shareClicker.addEventListener('click', (event) => {
   event.preventDefault();
   if (sectionShare.classList.contains('collapsed')) {
-    showShareClicker();
+    sectionShare.classList.remove('collapsed');
     arrowShare.classList.add('arrow-up');
     arrowShare.classList.remove('arrow-down');
     closeDesign();
     closeFill();
-    sectionFill.classList.add('collapsed');
-    sectionDesign.classList.add('collapsed');
-  } else {
-    hideShareClicker();
-    arrowShare.classList.remove('arrow-up');
-    arrowShare.classList.add('arrow-down');
-    sectionFill.classList.remove('collapsed');
-    sectionDesign.classList.remove('collapsed');
   }
-}
-
-// ---------- FUNCIÓN BOTÓN (COLOCAR Y REVISAR) -----------------
+});
 
 createCardBtn.addEventListener('click', (event) => {
   event.preventDefault();
@@ -118,8 +62,3 @@ function closeFill() {
   arrowFill.classList.add('arrow-down');
   arrowFill.classList.remove('arrow-up');
 }
-// ---------- EVENTOS CLICK -----------------
-
-designClicker.addEventListener('click', handleClickDesing);
-fillClicker.addEventListener('click', handleClickFill);
-shareClicker.addEventListener('click', handleClickShare);
