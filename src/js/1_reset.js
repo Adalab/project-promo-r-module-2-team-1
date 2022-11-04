ca7a172c132883dd900c2f6f06d1e97b4dc6dcbb
lauragonzalezcalvo <laura24gc@gmail.com>
2022-11-04 08:19:31 +0100

    Doing de btnReset, ticket03

--- src/js/1_reset.js
+++ src/js/1_reset.js
@@ -1,9 +1,25 @@
 'use strict';
-/*resetBtn.addEventListener('click',(event) => {
-      event.preventDefault();
-      profileName.innerHTML =
-  })*/
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 
-function resetBtnClick(){
 
-};
+
+function resetBtnClick (event) {
+  event.preventDefault();
+  // cardArticlePreview.classList.add('palette-1');
+  // cardArticlePreview.classList.remove('palette-2');
+  // cardArticlePreview.classList.remove('palette-3');
+  paletteRadio1.checked; //no se queda por defecto la primera clicada
+  inputName.value = '';
+  inputJob.value = '';
+  fileField.value = '';
+  profileImage.style.backgroundImage = '';
+  profilePreview.style.backgroundImage = '';// se queda por defecto la que hemos puesto
+  inputEmail.value = '';
+  inputPhone.value = '';
+  inputLinkedin.value = '';
+  inputGithub.value = '';
+  updatePreview();
+}
+
+resetBtn.addEventListener('click', resetBtnClick );
